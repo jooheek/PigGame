83a1ae34f14c492f6e8a0b0fa15fe683dfa390cb
jooheek <kimjoohee713@gmail.com>
2020-09-16 00:20:48 +0900

    pig game finished

--- src/main/resources/app.js
+++ src/main/resources/app.js
@@ -7,53 +7,52 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 */
 
-var scores,roundScore,activePlayer;
+var scores,roundScore,activePlayer,gamePlaying;
 
- scores =[0,0];
- roundScore = 0;
- activePlayer =0;
-
-document.querySelector('.dice').style.display='none';
-
-document.getElementById('score-0').textContent ='0';
-document.getElementById('score-1').textContent ='0';
-document.getElementById('current-0').textContent ='0';
-document.getElementById('current-1').textContent ='0';
-
-//getElementById가 queryselector보다 빠르다
+init();
 
 document.querySelector('.btn-roll').addEventListener('click',function(){
+    if(gamePlaying){
+        //1.Random number
+        var dice = Math.floor(Math.random()*6)+1;
 
-    //1.Random number
-    var dice = Math.floor(Math.random()*6)+1;
+        //2.display the result
+        var diceDOM = document.querySelector('.dice');
+        diceDOM.style.display='block';
+        diceDOM.src='dice-'+dice+'.png';
 
-    //2.display the result
-    var diceDOM = document.querySelector('.dice');
-    diceDOM.style.display='block';
-    diceDOM.src='dice-'+dice+'.png';
-
-    //3.update the round score id the rolled number was not a 1
-    if(dice !== 1){
-        //add score
-        roundScore += dice;
-        document.querySelector('#current-'+activePlayer).textContent = roundScore;
-    }else{
-        nextPlayer();
+        //3.update the round score id the rolled number was not a 1
+        if(dice !== 1){
+            //add score
+            roundScore += dice;
+            document.querySelector('#current-'+activePlayer).textContent = roundScore;
+        }else{
+            nextPlayer();
+        }
     }
+
 });
 
 document.querySelector('.btn-hold').addEventListener('click',function (){
+    if(gamePlaying){
+        //add current score to global score
+        scores[activePlayer] += roundScore;
 
-    //add current score to global score
-    scores[activePlayer] += roundScore;
+        //update the ui
+        document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
 
-    //update the ui
-    document.querySelector('#score-'+activePlayer).textContent = scores[activePlayer];
-
-    //check if player won the game
-
-    //Next player
-    nextPlayer();
+        //check if player won the game
+        if(scores[activePlayer]>=100){
+            document.querySelector('#name-'+activePlayer).textContent='Winner!';
+            document.querySelector('.dice').style.display='none';
+            //css가 많다면 이런 식의 코드는 부적합하다
+            document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+            document.querySelector('.player-'+activePlayer+'-panel').classList.remove('winner');
+            gamePlaying=false;
+        }else{
+            nextPlayer();
+        }
+    }
 
 });
 
@@ -75,6 +74,27 @@ function nextPlayer(){
 
 }
 
+    document.querySelector('.btn-new').addEventListener('click',init);
+//init()대신 init을 작성하는 이유 init함수를 무조건 실행하고 싶지 않기 때문
+function init(){
+    scores = [0, 0];
+    activePlayer=0;
+    roundScore=0;
+    gamePlaying = true;
+
+    document.querySelector('.dice').style.display='none';
+    document.getElementById('score-0').textContent ='0';
+    document.getElementById('score-1').textContent ='0';
+    document.getElementById('current-0').textContent ='0';
+    document.getElementById('current-1').textContent ='0';
+    document.querySelector('#name-0').textContent='Player 1';
+    document.querySelector('#name-1').textContent='Player 2';
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+}
 
 // document.querySelector('#current-'+activePlayer).textContent = dice;
 // querySelector : 찾으려는 String의 가장 첫번째 값을 가져온다
